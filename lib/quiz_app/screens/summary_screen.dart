3d4270e786194d5f59a114be192adcec5b6120d1
TOUFIK <toufik.fouad@gmail.com>
2026-04-14 17:18:17 -0700

    add safe area widget

--- lib/quiz_app/screens/summary_screen.dart
+++ lib/quiz_app/screens/summary_screen.dart
@@ -11,44 +11,45 @@ class SummaryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            for (UserAnswer u in answers)
-              Column(
-                children: [
-                  Text(
-                    u.question.text,
-                    style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center,
-                  ),
-                  for (Answer a in u.question.answers)
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: a.isCorrect
-                              ? Colors.green
-                              : (a == u.answer ? Colors.red : Colors.purple),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Center(
-                          child: Text(
-                            a.text,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (UserAnswer u in answers)
+                Column(
+                  children: [
+                    Text(
+                      u.question.text,
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                    for (Answer a in u.question.answers)
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: a.isCorrect
+                                ? Colors.green
+                                : (a == u.answer ? Colors.red : Colors.purple),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              a.text,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                ],
-              ),
-          ],
+                  ],
+                ),
+            ],
+          ),
         ),
       ),
     );
